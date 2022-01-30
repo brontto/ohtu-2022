b30a19744df0a311ad0b7028ef45ea8fec691f2f
brontto <bronttosaur@gmail.com>
2022-01-30 15:10:03 +0200

    viikko3tehtävä2

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,5 +1,6 @@
 import requests
 from player import Player
+import player
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
@@ -15,14 +16,18 @@ def main():
             player_dict['name'],
             player_dict['team'],
             player_dict['goals'],
-            player_dict['assists']
+            player_dict['assists'],
+            player_dict['nationality']
         )
 
         players.append(player)
 
     print("Oliot:")
+    
+    finplayers = sorted(list(filter(lambda x: (x.nationality == "FIN"), players)), key=lambda x: x.points(),reverse=True)
 
-    for player in players:
+    
+    for player in finplayers:
         print(player)
 
 if __name__ == "__main__":
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -2,11 +2,15 @@ from re import template
 
 
 class Player:
-    def __init__(self, name, team, goals, assists):
+    def __init__(self, name, team, goals, assists, nationality):
         self.name = name
         self.team = team
         self.goals = goals
         self.assists = assists
+        self.nationality = nationality
+    
+    def points(self):
+        return self.goals+self.assists
     
     def __str__(self):
-        return self.name + " team " + self.team + " goals " + str(self.goals) + " assists " + str(self.assists)
+        return f"{self.name:20}" + f"{self.team:5}" + f"{str(self.goals):2}" + " + " + f"{str(self.assists):2}" + " = " + f"{str(self.goals+self.assists)}"
