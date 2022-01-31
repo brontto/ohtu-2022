b9afa27f203af206f59f15477e36be8ea58c2b4f
brontto <bronttosaur@gmail.com>
2022-01-31 15:42:27 +0200

    kahden saman tuotteen hinta oikein

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -39,3 +39,19 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(banaani)
         
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+        
+    def test_kahde_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tarvaraa(self):
+        maito = Tuote("Maito", 3)
+        
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+        
+    def test_kahde_saman_tuotteen_lisaamisen_jalkeen_hinta_oikein(self):
+        maito = Tuote("Maito", 3)
+        
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        
+        self.assertEqual(self.kori.hinta(), 6)
